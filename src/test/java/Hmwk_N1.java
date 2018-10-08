14e7088d92f604b5d5c936d7027165126539ac7a
Dmitrijs Balickis <workaholiclv@gmail.com>
2018-10-08 10:31:49 +0300

    Add files via upload
    Client is taking loan for 30 years. We have amount client needs. We know that he will need to pay 10% for the first 10 years, 8% for the second 10 years and 6% for the last 10 years.

---
+++ src/test/java/Hmwk_N1.java
@@ -0,0 +1,24 @@
+﻿import org.junit.jupiter.api.Test;
+
+public class Hmwk_N1 {
+
+    private String text = " EUR. Interest to pay for ";
+
+    @Test
+    public void interest2Pay() {
+
+        intrstAmountPrint(30000, 10);
+        intrstAmountPrint(30000, 8);
+        intrstAmountPrint(30000, 6);
+
+    }
+
+    private void intrstAmountPrint(Integer a, Integer b) {
+        Integer c = a * b / 100;
+        System.out.println("Loan " + a + text + b + "% is: " + c + " EUR");
+
+    }
+      public static void main(String[] args){
+        new Hmwk_N1().interest2Pay();
+    }
+}
