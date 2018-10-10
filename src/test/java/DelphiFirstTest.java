8e127e1357793f0a83a2853b630ca693db7f0a83
Dmitrijs <d.balickis@gmail.com>
2018-10-10 20:56:08 +0300

    03.10.2018 commit

--- src/test/java/DelphiFirstTest.java
+++ src/test/java/DelphiFirstTest.java
@@ -1,8 +1,28 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class DelphiFirstTest {
+
+    private final By ARTICLE_TITLE =  By.xpath(".//a[@class = 'top2012-title']");
+
+    /**
+     * This test will test first article title presents on page
+     */
     @Test
     public void delphiFirstTitleTet() {
+        System.setProperty("webdriver.chrome.driver","/Users/user/Downloads/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().fullscreen();
+        driver.get("http://delfi.lv");
 
+        String articleToCheck ="'OlyBet' basketbola līga: LU - 'Kalev/Cramo'. Video tiešraide";
+        WebElement article = driver.findElement(ARTICLE_TITLE);
+        String articleText = article.getText();
+
+        Assertions.assertEquals(articleToCheck, articleText,"Fatal error");
     }
 }
