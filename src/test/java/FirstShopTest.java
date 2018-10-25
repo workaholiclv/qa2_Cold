f54fcba02ed33aeb379ce6b4a647d64620b43be2
Dmitrijs Balickis <workaholiclv@gmail.com>
2018-10-25 09:59:48 +0300

    Update FirstShopTest.java

--- src/test/java/FirstShopTest.java
+++ src/test/java/FirstShopTest.java
@@ -8,11 +8,13 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.List;
 
+//Homework Nr.3.
+
 public class FirstShopTest {
 
-    //Выносим WebDriver на уровень класса, что бы он был доступен во всех методах этого класса
+    // Taking WebDriver to class level so it will be visible in all of the methods of this class
     private WebDriver driver;
-
+    // Variables with web shop address and menu xPathes
     private final String HOME_PAGE = "http://automationpractice.com";
     private final By MAIN_MENU_ITEMS = By.xpath(".//ul[@class = 'sf-menu clearfix menu-content sf-js-enabled sf-arrows']/li/a");
     private final By FILTER_CHECK_BOX = By.xpath(".//ul[@class = 'col-lg-12 layered_filter_ul']/li/label");
@@ -20,7 +22,7 @@ public class FirstShopTest {
     private final By LOADER = By.xpath(".//ul[@class = 'product_list grid row']/p");
     private final By LIST_VIEW_ICON = By.className("icon-th-list");
 
-
+    // Adding Junit component test
     @Test
     public void dressesTest() {
 
@@ -29,10 +31,10 @@ public class FirstShopTest {
         driver.manage().window().fullscreen();
         driver.get(HOME_PAGE);
 
-        //код, который потом может быть использован в других тестах, выносим в отдельный метод, который потом просто вызываем подавая параметры
+        // Selecting menu item "WOMEN". Code wich could be reusable in other places we can write in separate method which we can call by parameters
         selectMenuItem("WOMEN");
 
-        //здесь вызываем метрод, который заселектит нужный нам чек бокс в определённом фильтре
+        // Calling method which will select check-box in filter
         selectFilter("Dresses", FILTER_CHECK_BOX);
         waitLoad();
 
@@ -42,15 +44,14 @@ public class FirstShopTest {
         driver.findElement(LIST_VIEW_ICON).click();
     }
 
-    //Этот метод может использоваться для выбора любого пункта основного менюб надо его только передать как параметр
-
+    // This method could be used to choose any item from main menu, we only need to pass it as parameter
     private void selectMenuItem(String menuItem) {
         List<WebElement> menuItems = driver.findElements(MAIN_MENU_ITEMS);
 
-        //цикл: принимает 3 параметра: с какого элемента обходим, по какой элемент, с каким шагом
-        //если не знаем количество элементов в списке - метод .size возвращает кол-во элементов
+        // This cicle is receiving 3 parameters: from which element entering to which and with what step (i++)
+        // If we don't know element count in list, then we need to use function ".size" which will return element count
         for (int i = 0; i < menuItems.size(); i++) {
-            //.equals - в случае работы со строками - проверяет на полное совпадение строк
+            // Function ".equals" - if we're working with string, checks for full string conjunction
             if (menuItems.get(i).getText().equals(menuItem)) {
                 menuItems.get(i).click();
                 break;
@@ -62,12 +63,12 @@ public class FirstShopTest {
 
         List<WebElement> filterItems = driver.findElements(locator);
 
-        //аналог цикла for - данная запись должна быть использована если обходим все элементы списка
-        //1. Указываем с какими объектами работаем - WebElement (именно из них состоит список, который обходим)
-        //2. Указываем название - через это название мы раблтаем с каждым элементом списка
-        //3. После двоеточия указываем какой список обходим
+        // Сicle for analogue - this entry should be used if bypass all list elements
+         // 1. Specify which objects we work with - WebElement (the list consists of elements which we bypass)
+         // 2. Specify the name - through this name we work with each element of the list
+         // 3. After the colon, we indicate which list we are bypasing
         for (WebElement filterItem : filterItems) {
-            //.contains - в случае работы со строками - проверяет на частичное совпадение строк
+            //Function ".contains" - in case of working with string is checking for partial conjunction of string
             if (filterItem.getText().contains(filterName)) {
                 filterItem.click();
                 break;
@@ -75,8 +76,9 @@ public class FirstShopTest {
         }
     }
 
-    //Ожидание, указываем "окно браузера" в котором ждём, таимаут - сколько максимум времени ждём, далее указываем условие которое ожидаем
-    //Селениум будет постоянно проверять, выполнилось ли условие, если да - не ждём до конца - идём дальше - так экономим время
+   // Waiting - adding browser window in which we are waiting for result, timeout - how long we'll wait, then adding condition which we expect. 
+   // Selenium will be constantly checking whether condition is met, if yes, then we don't have to wait and can move forward, saving our time.
+   
     private void waitLoad() {
         new WebDriverWait(driver, 10).until(ExpectedConditions.invisibilityOfElementLocated(LOADER));
     }
