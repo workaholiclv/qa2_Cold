a28bed9d9d623a30110ff135424c0c28d19c1ff5
Dmitrijs Balickis <workaholiclv@gmail.com>
2018-10-25 09:36:40 +0300

    Update DelfiTitleAndCommentCountComparison.java

--- src/test/java/DelfiTitleAndCommentCountComparison.java
+++ src/test/java/DelfiTitleAndCommentCountComparison.java
@@ -2,7 +2,7 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.util.List;
 
