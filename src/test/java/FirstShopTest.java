6edcdd1614b901bb6107c824a0cb2c1d0d3720d0
Dmitrijs Balickis <workaholiclv@gmail.com>
2018-11-05 11:14:59 +0200

    Update FirstShopTest.java

--- src/test/java/FirstShopTest.java
+++ src/test/java/FirstShopTest.java
@@ -1,6 +1,6 @@
-import org.junit.Test;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -10,14 +10,11 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.List;
 
-//Homework Nr.3.
-
 public class FirstShopTest {
 
     // Taking WebDriver to class level so it will be visible in all of the methods of this class
     private WebDriver driver;
-    
-    // Variables with web shop address and menu xPathes
+
     private final String HOME_PAGE = "http://automationpractice.com";
     private final By MAIN_MENU_ITEMS = By.xpath(".//ul[@class = 'sf-menu clearfix menu-content sf-js-enabled sf-arrows']/li/a");
     private final By FILTER_CHECK_BOX = By.xpath(".//ul[@class = 'col-lg-12 layered_filter_ul']/li/label");
@@ -26,20 +23,20 @@ public class FirstShopTest {
     private final By LIST_VIEW_ICON = By.className("icon-th-list");
     private final Logger LOGGER = LogManager.getLogger(FirstShopTest.class);
 
-    // Adding Junit component test
+
     @Test
     public void dressesTest() {
-        LOGGER.info("Setting Chrome driver global property");
-        System.setProperty("webdriver.chrome.driver","/Users/user/Downloads/chromedriver");
-        
-        LOGGER.info("Open browser");
-        WebDriver driver = new ChromeDriver();
-        driver.manage().window().fullscreen();
-        
+        LOGGER.info("Setting Gecko global property");
+        System.setProperty("webdriver.chrome.driver", "/driver/chromedriver.exe");
+
+        LOGGER.info("openPage browser");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
         LOGGER.info("Openning home page");
         driver.get(HOME_PAGE);
 
-        // Selecting menu item "WOMEN". Code wich could be reusable in other places we can write in separate method which we can call by parameters
+// Selecting menu item "WOMEN". Code wich could be reusable in other places we can write in separate method which we can call by parameters
         selectMenuItem("WOMEN");
 
         // Calling method which will select check-box in filter
@@ -48,21 +45,22 @@ public class FirstShopTest {
 
         selectFilter("Orange", COLOR_FILTER);
         waitLoad();
-        
+
         LOGGER.info("Click to view icon");
         driver.findElement(LIST_VIEW_ICON).click();
     }
 
     // This method could be used to choose any item from main menu, we only need to pass it as parameter
+
     private void selectMenuItem(String menuItem) {
-         LOGGER.info("Selecting" +
+        LOGGER.info("Selecting" +
                 "menuItem");
         List<WebElement> menuItems = driver.findElements(MAIN_MENU_ITEMS);
 
         // This cicle is receiving 3 parameters: from which element entering to which and with what step (i++)
         // If we don't know element count in list, then we need to use function ".size" which will return element count
         for (int i = 0; i < menuItems.size(); i++) {
-            // Function ".equals" - if we're working with string, checks for full string conjunction
+            //.equals - в случае работы со строками - проверяет на полное совпадение строк
             if (menuItems.get(i).getText().equals(menuItem)) {
                 menuItems.get(i).click();
                 break;
@@ -71,13 +69,14 @@ public class FirstShopTest {
     }
 
     private void selectFilter(String filterName, By locator) {
+
         LOGGER.info("Selecting from filter" +"filterName");
         List<WebElement> filterItems = driver.findElements(locator);
 
         // Сicle for analogue - this entry should be used if bypass all list elements
-         // 1. Specify which objects we work with - WebElement (the list consists of elements which we bypass)
-         // 2. Specify the name - through this name we work with each element of the list
-         // 3. After the colon, we indicate which list we are bypasing
+        // 1. Specify which objects we work with - WebElement (the list consists of elements which we bypass)
+        // 2. Specify the name - through this name we work with each element of the list
+        // 3. After the colon, we indicate which list we are bypasing
         for (WebElement filterItem : filterItems) {
             //Function ".contains" - in case of working with string is checking for partial conjunction of string
             if (filterItem.getText().contains(filterName)) {
@@ -87,9 +86,9 @@ public class FirstShopTest {
         }
     }
 
-   // Waiting - adding browser window in which we are waiting for result, timeout - how long we'll wait, then adding condition which we expect. 
-   // Selenium will be constantly checking whether condition is met, if yes, then we don't have to wait and can move forward, saving our time.
-   
+    // Waiting - adding browser window in which we are waiting for result, timeout - how long we'll wait, then adding condition which we expect.
+    // Selenium will be constantly checking whether condition is met, if yes, then we don't have to wait and can move forward, saving our time.
+
     private void waitLoad() {
         new WebDriverWait(driver, 10).until(ExpectedConditions.invisibilityOfElementLocated(LOADER));
     }
