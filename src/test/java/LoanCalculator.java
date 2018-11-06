9f7ea16e08d62660dfc955e50611a5e45af8a48a
Dmitrijs Balickis <workaholiclv@gmail.com>
2018-11-06 10:39:50 +0200

    Homework Nr.1.

--- src/test/java/LoanCalculator.java
+++ src/test/java/LoanCalculator.java
@@ -1,22 +1,27 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-//Homework Nr.1.
+// Homework Nr.1.
 
 public class LoanCalculator {
-    private int loanPeriod = 10;
     private int takenLoanAmount = 2500;
     private int totalAmountToPay = 13500;
 
-   @Test
-    public void loanCalculation() {
+    @Test
+    public void myTest() {
+
         double loanAmountForThirtyYears, firstTenYearsToPay, secondTenYearsToPay, thirdTenYearsToPay;
 
-        firstTenYearsToPay = (takenLoanAmount * (1 + loanPeriod * 0.1));
-        secondTenYearsToPay = (takenLoanAmount * (1 + loanPeriod * 0.08));
-        thirdTenYearsToPay = (takenLoanAmount * (1 + loanPeriod * 0.06));
+        firstTenYearsToPay = amountReceived(0.1);
+        secondTenYearsToPay = amountReceived(0.08);
+        thirdTenYearsToPay = amountReceived(0.06);
+        
         loanAmountForThirtyYears = firstTenYearsToPay + secondTenYearsToPay + thirdTenYearsToPay;
 
         Assertions.assertEquals(totalAmountToPay, loanAmountForThirtyYears, "Entered totalAmountToPay is incorrect!");
+    }
+
+    private double amountReceived(double percent) {
+        return takenLoanAmount * (1 + 10 * percent);
     }
 }
